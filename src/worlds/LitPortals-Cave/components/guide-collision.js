e91358b6d81cebdbd8ac3dd378bac3baa4197eb3
MsganaT <101358591+MsganaT@users.noreply.github.com>
2025-04-06 22:57:27 -0400

    added animation for when player enters/leaves cube

--- src/worlds/LitPortals-Cave/components/guide-collision.js
+++ src/worlds/LitPortals-Cave/components/guide-collision.js
@@ -1,7 +1,9 @@
 'use strict';
 
 AFRAME.registerComponent('guide-collision', {
-    schema: {},
+    schema: {
+        guideZone:{type: 'string', default:''}
+    },
 
     init: function () {
 
@@ -15,17 +17,39 @@ AFRAME.registerComponent('guide-collision', {
   
         playerEl= CIRCLES.getMainCameraElement();
         // console.log(playerEl);
+        // adding aframe physics components to player camera element
         playerEl.setAttribute('ammo-body',{ type:'kinematic', emitCollisionEvents: true});
         playerEl.setAttribute('ammo-shape',{ type: 'capsule', fit:'manual'});
   
-        playerEl.addEventListener("collidestart", function(e) {
-            console.log("Player has collided with body #" + e.detail.targetEl.id);
-            // console.log("Player has collided with body #");
+        // playerEl.addEventListener("collidestart", function(e) {
+        //     console.log("Player has collided with body #" + e.detail.targetEl.id);
+        //     // console.log("Player has collided with body #");
     
-            e.detail.targetEl; // Other entity, which playerEl touched.
-            });
+        //     e.detail.targetEl; // Other entity, which playerEl touched.
+        //     });
 
       });
+
+      Context_AF.el.addEventListener("collidestart", function(e) {
+        console.log("Object has been collided with by: " + e.detail.targetEl.id);
+        console.log(`Emitting animation event: ${Context_AF.data.guideZone}Start`);
+        document.querySelector(`#${Context_AF.data.guideZone}`).emit(`${Context_AF.data.guideZone}Start`, null, false);
+
+        // e.detail.targetEl; // Other entity, which touched.
+        // document.querySelector(data.guideZone).setAttribute('animation__grow', {property: 'scale', to: {x:5, y:5, z:5}, dur: 2000, easing: linear, loop: false, autoplay })
+    
+    });
+
+    Context_AF.el.addEventListener("collideend", function(e) {
+        console.log("Object is no longer colliding with: " + e.detail.targetEl.id);
+        console.log(`Emitting animation event: ${Context_AF.data.guideZone}End`);
+        document.querySelector(`#${Context_AF.data.guideZone}`).emit(`${Context_AF.data.guideZone}End`, null, false);
+
+        // e.detail.targetEl; // Other entity, which touched.
+        // document.querySelector(data.guideZone).setAttribute('animation__grow', {property: 'scale', to: {x:5, y:5, z:5}, dur: 2000, easing: linear, loop: false, autoplay })
+    
+    });
+      
   
 
         
